56774f321035ddb4fb9f26cdd4532df3cfd3e399
Cameron Ordone <ceordone@gmail.com>
2015-01-31 08:43:50 -0600

    Changed how code reads gyro.

--- src/libraries/SlideDrive.java
+++ src/libraries/SlideDrive.java
@@ -29,39 +29,12 @@ public class SlideDrive extends DriveMethod {
      * The {@link Gyro} that the {@link SlideDrive} uses for
      * field-oriented driving and keeping the correct orientation.
      */
-    protected final Gyro gyro;
+    protected final BetterGyro gyro;
 
-    /**
-     * The rotation speed below which the rotation PID controller is enabled.
-     * When the rotation speed increases above this value the controller is
-     * disabled to allow the robot to turn.
-     */
     public final double ROTATION_DEADBAND = 0.05;
-    
-    /**
-     * The proportional constant of the PID algorithm. This value is multiplied
-     * by the error.
-     */
     public static final double ROTATION_P = 0.01;
-    
-    /**
-     * The integral constant of the PID algorithm. This value is multiplied by
-     * the sum of the error over time. This is used to make the robot turn more
-     * if it is taking a long time to reach its target.
-     */
     public static final double ROTATION_I = 0.0;
-    
-    /**
-     * The derivative constant of the PID algorithm. This value is multiplied by
-     * the rate of change of the error.
-     */
     public static final double ROTATION_D = 0.0;
-    
-    /**
-     * The feed-forward constant of the PID algorithm. This value purely feeds 
-     * the command signal forward through the control loop, adding to the loop's output 
-     * based directly off of a command input without dependence on prior feedback.
-     */
     public static final double ROTATION_F = 0.0;
     private double rotationSpeedPID = 0.0;
     private double gyroOffset = 0.0;
@@ -76,7 +49,7 @@ public class SlideDrive extends DriveMethod {
      * @param gyro the {@link Gyro} to use for orientation correction and
      * field-oriented driving
      */
-    public SlideDrive(ThreeWheelDriveController controller, Gyro gyro) {
+    public SlideDrive(ThreeWheelDriveController controller, BetterGyro gyro) {
         super(controller);
         
         this.controller = controller;
@@ -147,7 +120,7 @@ public class SlideDrive extends DriveMethod {
         drive(speed, 0);
     }
 
-    public void slideDrive_Orientation(double x, double y, double angle) {
+    public void driveOrientation(double x, double y, double angle) {
         if (!rotationPIDController.isEnable() || rotationPIDController.getSetpoint() != angle) {
             rotationPIDController.setSetpoint(angle);
             rotationPIDController.enable();
@@ -188,36 +161,6 @@ public class SlideDrive extends DriveMethod {
     }
 
     /**
-     * Drives the robot at the specified speed in the direction specified as an
-     * angle in degrees while rotating. This does not take into account the gyro
-     * correction because we do not use it.
-     *
-     * @param magnitude The speed that the robot should drive in a given
-     * direction.
-     * @param direction the direction the robot should drive in degrees,
-     * independent of rotation
-     * @param rotation The rate of rotation for the robot that is completely
-     * independent of the magnitude or direction. [-1.0..1.0]
-     */
-    public void drivePolar(double magnitude, double direction, double rotation) {
-        // Normalized for full power along the Cartesian axes.
-        magnitude = DriveUtils.limit(magnitude) * sqrt(2.0);
-        // The rollers are at 90 degree angles.
-        double dirInRad = (direction + 90.0) * PI / 180.0;
-        double cosD = cos(dirInRad);
-        double sinD = sin(dirInRad);
-
-        double wheelSpeeds[] = new double[3];
-        wheelSpeeds[0] = (sinD * magnitude + rotation); // Left speed
-        wheelSpeeds[1] = (sinD * magnitude - rotation); // Right speed
-        wheelSpeeds[2] = (cosD * magnitude); // Strafe speed
-
-        DriveUtils.normalize(wheelSpeeds);
-
-        controller.drive(wheelSpeeds[0], wheelSpeeds[1], wheelSpeeds[2]);
-    }
-
-    /**
      * Gets the corrected rotation speed based on the gyro heading and the
      * expected rate of rotation. If the rotation rate is above a threshold, the
      * gyro correction is turned off.
@@ -241,7 +184,7 @@ public class SlideDrive extends DriveMethod {
             // If the rotation rate is less than the deadband, turn on the PID
             // controller and set its setpoint to the current angle.
             if (abs(rotationSpeed) < ROTATION_DEADBAND) {
-                gyroOffset = gyro.getAngle();
+                gyroOffset = gyro.getYaw();
                 rotationPIDController.setSetpoint(gyroOffset);
                 rotationPIDController.enable();
             }
