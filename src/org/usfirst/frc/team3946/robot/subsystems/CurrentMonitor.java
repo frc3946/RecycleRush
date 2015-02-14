95bb6b3f9db2b1a7f97c113ea927880981bb91c3
Jeffery Wise <jrwise@gmail.com>
2015-02-14 11:34:47 -0600

    updated CurrentMonitor

--- src/org/usfirst/frc/team3946/robot/subsystems/CurrentMonitor.java
+++ src/org/usfirst/frc/team3946/robot/subsystems/CurrentMonitor.java
@@ -9,7 +9,7 @@ public class CurrentMonitor extends Subsystem {
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
-
+	//PowerDistributionPanel 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
